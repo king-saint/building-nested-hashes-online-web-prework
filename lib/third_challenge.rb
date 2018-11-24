bad2b44e71cb5ee0b031f04cf86e663539c27318
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-24 01:13:25 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,24 +11,24 @@ def third_challenge
         },
       :hero => {
         name => "Romeo",
-        :age => 15,
-        :status => "alive"
+        age => 15,
+        status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet",
-        :age => "50"
+        name => "Lord Capulet",
+        age => "50"
         },
       :matriarch => {
-        :name => "Lady Capulet",
-        :age => "51"
+        name => "Lady Capulet",
+        age => "51"
         },
       :heroine => {
-        :name => "Juliet",
-        :age => "15",
-        :status => "alive"
+        name => "Juliet",
+        age => "15",
+        status => "alive"
         },
       :heroine_friends => []
    }
