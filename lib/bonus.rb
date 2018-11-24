58e41b59289aca797193ddfcd403f0e15aee8e19
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-24 01:26:48 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,8 @@ def bonus
    }
   }
 
-epic_tragedy[][][] = "dead"
-epic_tragedy[:heroine][:name][]"dead"
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status]"dead"
   
 
  
