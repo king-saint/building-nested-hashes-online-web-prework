5ead02f3d32b2a55c679e969bba76acdb76a7d2e
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-24 01:14:34 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,11 +2,11 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name => "Lord Montague",
-        age => "53"
+        name: "Lord Montague",
+        age: "53"
         },
       :matriarch => {
-        name => "Lady Montague",
+        name: "Lady Montague",
         age => "53"
         },
       :hero => {
