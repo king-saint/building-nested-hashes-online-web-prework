d8d7df4dcb8f7fd81cc2007f1efe508b2871d56d
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-24 01:25:54 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,10 +20,10 @@ def bonus
    }
   }
 
-  #code your solution here:
-
+epic_tragedy[][][] = "dead"
+epic_tragedy[:heroine"dead"
   
 
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+ 
   epic_tragedy
 end
